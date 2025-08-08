b7733d6dfd82b00cb085aed4d39529ec1a8f9c93
Celso Junior Ladislau <celso.junior@autvix.com.br>
2025-08-08 10:03:32 -0300

    feat: Conteúdo da aula 2 - Intrudução ao desenvolvimento WEB *Criação de mais uma rota para a api e crição de teste para a mesma

--- fast_zero/app.py
+++ fast_zero/app.py
@@ -1,8 +1,27 @@
+from http import HTTPStatus
+
 from fastapi import FastAPI
+from starlette.responses import HTMLResponse
+
+from fast_zero.schemas import Message
 
 app = FastAPI()
 
 
-@app.get('/')
+@app.get('/', status_code=HTTPStatus.OK, response_model=Message)
 def read_root():
     return {'message': 'Olá Mundo'}
+
+
+@app.get('/exercicio-1-html', response_class=HTMLResponse)
+def exercicio_1_html_aula_2():
+    return """
+    <html>
+        <head>
+            <title>Exercício 1</title>
+        </head>
+        <body>
+            <h1>Olá Mundo</h1>
+        </body>
+    </html>
+    """
--- tests/test_app.py
+++ tests/test_app.py
@@ -6,9 +6,18 @@ from fast_zero.app import app
 
 
 def test_root_deve_retornar_ok_e_ola_mundo():
-    client = TestClient(app)
+    client = TestClient(app) # Arrange (Organizar - fase 1 do teste)
 
-    response = client.get('/')
+    response = client.get('/') # Act (Agir - fase 2 do testo)
 
-    assert response.status_code == HTTPStatus.OK
-    assert response.json() == {'message': 'Olá Mundo'}
+    assert response.status_code == HTTPStatus.OK # Assert (Afirmar - fase 3 do teste)
+    assert response.json() == {'message': 'Olá Mundo'} # Assert (Afirmar - fase 3 do teste)
+
+
+def test_root_deve_retornar_html():
+    client = TestClient(app) # Arrange (Organizar - fase 1 do teste)
+
+    response = client.get('/exercicio-1-html') # Act (Agir - fase 2 do testo)
+
+    assert response.status_code == HTTPStatus.OK # Assert (Afirmar - fase 3 do teste)
+    assert '<h1>Olá Mundo</h1>' in response.text # Assert (Afirmar - fase 3 do teste)
