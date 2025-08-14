f337c9e6ca2094f52c0f328e9afeeef655294c05
celso.junior <celso.junior@autvix.com.br>
2025-08-14 14:57:14 -0300

    Feat: estudo de 50% da aula 4, parei aqui:'Criando uma Fixture para interações com o Banco de Dados'

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,22 @@
+from fast_zero.models import table_registry
 import pytest
 from fastapi.testclient import TestClient
 
 from fast_zero.app import app
+from sqlalchemy import create_engine, table
+from sqlalchemy.engine import create
 
 
 @pytest.fixture
 def client():
     return TestClient(app)
+
+@pytest.fixture
+def session():
+    engine = create_engine('sqlite:///:memory:')
+    table_registry.metadata.create_all(engine)
+
+    with Session(engine) as session:
+        yield session
+
+    table_registry.metadata.drop_all(engine)
