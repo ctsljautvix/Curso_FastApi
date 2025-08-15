0f9c33d10c22966e232e139a56c42c97e4391f67
celso.junior <celso.junior@autvix.com.br>
2025-08-15 15:22:38 -0300

    Feat: Continuação da 4 aula do curso, estou nessa parte: Analisando a migração automática

--- fast_zero/models.py
+++ fast_zero/models.py
@@ -2,16 +2,18 @@ from datetime import datetime
 
 from sqlalchemy import func, true
 from sqlalchemy.orm import Mapped, mapped_column, registry
-from sqlalchemy.sql import false
 
-table_registry =  registry
+table_registry = registry()
+
 
 @table_registry.mapped_as_dataclass
 class User:
-    __tablename__= 'users'
+    __tablename__ = 'users'
 
     id: Mapped[int] = mapped_column(init=False, primary_key=true)
     username: Mapped[str] = mapped_column(unique=true)
     password: Mapped[str]
     email: Mapped[str] = mapped_column(unique=true)
-    created_at: Mapped[datetime] = mapped_column(init=false, server_default=func.now())
+    created_at: Mapped[datetime] = mapped_column(
+        init=False, server_default=func.now()
+    )
